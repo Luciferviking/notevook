dfb49ffd26265798fb9d75a03a9b3ad128b5af40
Dinku <luciferdas1234@gmail.com>
2025-01-12 20:10:20 +0530

    Create, read, update
    these methods are defined solidly without leaving undefined holes in the array

--- src/app/script/method.js
+++ src/app/script/method.js
@@ -3,14 +3,23 @@ import { promises as fs } from 'fs';
 
 const filePath = path.join(process.cwd(), '/data/mockData.json');
 const fileDataStr = await fs.readFile(filePath, 'utf-8');
-const fileData = JSON.parse(fileDataStr);
+const fileData = JSON.parse(fileDataStr); // stores the whole parsed data in efficient
 
 //console.log(fileData);
 
+//search the id in the whole array and return the id's object
+function search(id_){
+    for(var x in fileData){
+        const itrId = (fileData[x].id);
+        if(itrId === id_){
+          //console.log(fileData[x]);
+          return x;
+        }
+    }
+}
 
-
-export function create(title_, content_){
-    console.log(typeof(fileData));
+export async function create(title_, content_){
+    //console.log(typeof(fileData));
 
     fileData.push({
         id: fileData.length + 1,
@@ -18,11 +27,29 @@ export function create(title_, content_){
         content: content_
     });
 
-    return(console.log(fileData));
+    await fs.writeFile(filePath, JSON.stringify(fileData, null, 2), 'utf-8'); // rewrites the whole file instead of appending therefore process in efficient
+    //return(console.log(fileData));
 }
-
-export function read(){}
-
-export function update(){}
+// reads the Id object
+export function read(id_){
+    //search func
+    const idIndex = search(id_);
+    //console.log(fileData[idIndex]);
+    return(fileData[idIndex]);
+}
+ 
+export function update(id_, whatKey, whaContent){
+    const workingObj = fileData[search(id_)];
+    switch(whatKey){
+        case "title": 
+            workingObj.title = whaContent;
+            break;
+        case "content":
+            workingObj.content = whaContent;
+            break;
+    }
+    fs.writeFile(filePath, JSON.stringify(fileData, null, 2), 'utf-8');
+    //console.log(fileData[search(id_)]);
+}
 
 export function remove(){}
--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-import { create } from "../script/method";
-create("lmao", "lorem");
+import { create, read, update } from "../script/method";
+
+//create("lmao", "lorem");
+//read(3);
+update(5, "title", "harry putter");
 
 export default function Test() {
   return <div>page</div>;
