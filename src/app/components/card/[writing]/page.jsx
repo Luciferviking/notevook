ccbab5858b86cd70b0c25992f2be734d03b36e91
Dinku <luciferdas1234@gmail.com>
2025-01-14 23:38:16 +0530

    Revert "made an add api"
    This reverts commit 441b8c9745c44a87614d3b8c7ccda8f7e287ce44.

--- src/app/components/card/[writing]/page.jsx
+++ src/app/components/card/[writing]/page.jsx
@@ -1,7 +1,27 @@
 export default async function Page({ params }) {
   // Await params if it's a Promise
   const resolvedParams = await params;
+  const { writing } = resolvedParams;
   const valueOfSlugInt = parseInt(resolvedParams.writing, 10);
 
-  return <div>hello {valueOfSlugInt}</div>;
+  // console.log(valueOfSlug);
+  // console.log(typeof valueOfSlug);
+
+  // console.log(read(valueOfSlug));
+
+  const readObj = read(valueOfSlugInt);
+  // console.log(readObj);
+
+  const ListItems = () => (
+    <div key={readObj.id} id={styles.mainCont}>
+      <p id={styles.pTest}>Checking CSS connectivity</p>
+      <div id={styles.titleCont}>{readObj.content}</div>
+    </div>
+  );
+
+  return (
+    <div>
+      <ListItems />
+    </div>
+  );
 }
